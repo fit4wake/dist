49eeaef228c8e3d0e7c104b09f8f56c164fea287
Nikolai Kaverin <nkaverin86@gmail.com>
2025-05-31 16:12:53 +0700

    Initial commit

---
+++ assets/channel-CYp2_9bt.js
@@ -0,0 +1,1 @@
+import{U as o,T as r}from"./markdownExport-BM1QXBIX.js";const a=(a,s)=>o.lang.round(r.parse(a)[s]);export{a as c};
---
+++ sw.js
@@ -0,0 +1,13 @@
+const CACHE_NAME = 'tea-chair-v1.0.0';
+const urlsToCache = [
+  '/manifest.json',
+  '/icon-192x192.png',
+  '/icon-512x512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
